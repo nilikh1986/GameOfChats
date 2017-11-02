7deb4f463844d01b58fa43d5a4658f2bb52665ab
Patnayak, Nilikh <Nilikh.Patnayak@searshc.com>
2017-11-02 11:31:38 -0500

    Changes for setting subview constraint as per safe area.

--- GameOfChats/GameOfChats/Controllers/LoginViewController.swift
+++ GameOfChats/GameOfChats/Controllers/LoginViewController.swift
@@ -12,13 +12,18 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        //view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
     
 
@@ -32,4 +37,15 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+    func blueColor() -> UIColor {
+        return UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+    }
 }
--- GameOfChats/GameOfChats/Controllers/ViewController.swift
+++ GameOfChats/GameOfChats/Controllers/ViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        self.view.backgroundColor = .red
+        
+        /*
+         
+         // Example of setting contraint of subview using safe area and without safe area.
+         
+        let tempView = UIView()
+        tempView.backgroundColor = .green
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tempView)
+        
+        NSLayoutConstraint.activate([
+            tempView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tempView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tempView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tempView.heightAnchor.constraint(equalToConstant: 100)
+            ])
+        // If navigation bar is hidden then top of temp view will start from safe area of view.
+        // If navigation bar is not hidden then top of temp view will start from navigation bar bottom.
+        //navigationController?.navigationBar.isHidden = true
+     */
     }
     
     func handleLogout() {
         print("logout tapped")
-        let loginController = LoginViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginController = storyboard.instantiateViewController(withIdentifier: "LoginController")
         present(loginController, animated: true, completion: nil)
     }
 }
